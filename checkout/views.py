f74e6d1c1c4f88eb80f394f351f66c3cb817f31b
Siobhanmcc <siobhanmccauley@gmail.com>
2021-06-01 14:35:49 +0000

    Added stripe js

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ix85GAkgpOpq9ZGWTB6EA8tcwLPr6PHQIyhDnq7Q86hPpGzCaV9hAr0DaSfD4zg11ajIpaaBkannPFSf3dgAT2S004nRTiqPj',
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
--- ms4/settings.py
+++ ms4/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ms4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -70,8 +76,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                  'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+                ]
         },
     },
 ]
