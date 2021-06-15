5934d60416c2145aacfdfbfc7875f1c08e3c4582
Siobhanmcc <siobhanmccauley@gmail.com>
2021-06-15 15:53:39 +0000

    Added new superuser

--- ms4/settings.py
+++ ms4/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -48,7 +49,6 @@ INSTALLED_APPS = [
     # Other
     'crispy_forms',
     'storages',
-
 ]
 
 MIDDLEWARE = [
@@ -196,6 +196,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
