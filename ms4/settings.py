1784be9b314a1b9adf0187c899b45d565e586f64
Siobhanmcc <siobhanmccauley@gmail.com>
2021-06-16 12:37:12 +0000

    Changes to homepage background in css

--- ms4/settings.py
+++ ms4/settings.py
@@ -119,6 +119,7 @@ WSGI_APPLICATION = 'ms4.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
+    print("Loading postgres database.....")
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
