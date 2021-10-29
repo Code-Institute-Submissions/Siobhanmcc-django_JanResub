6a7ae5564c628fb25e037e3d2ae61c0adb973104
Siobhanmcc <siobhanmccauley@gmail.com>
2021-10-29 20:49:47 +0000

    Update to home/views.py

--- home/views.py
+++ home/views.py
@@ -1,14 +1,18 @@
 from products.models import Product
+from django.shortcuts import render
 
 def index(request):
     """A view to return to the index page"""
 
     products =Product.objects.all()
-    selected_products = list(products.slice(3))
-    print(selected_products)
+    products_list = list(products)
+    selected_products = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    x=slice(3)
+    new_list = products_list[x]
+    print(new_list)
 
     context = {
-        'product': product,
+        'new_list': products,
     }
 
     return render(request, 'home/index.html')
