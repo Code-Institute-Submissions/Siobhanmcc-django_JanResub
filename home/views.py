abc592fbe19897908dda5fdbae3c901fa7977387
Siobhanmcc <siobhanmccauley@gmail.com>
2021-10-29 13:25:00 +0000

    Update to home/views.py

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+from products.models import Product
 
 def index(request):
-    """ A view to return the index page """
+    """A view to return to the index page"""
+
+    products =Product.objects.all()
+    selected_products = list(products.slice(3))
+    print(selected_products)
+
+    context = {
+        'product': product,
+    }
 
     return render(request, 'home/index.html')
