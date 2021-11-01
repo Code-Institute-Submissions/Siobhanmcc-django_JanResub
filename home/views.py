1a4e3f9878d5c2b6859367f5b012886a5017b307
Siobhanmcc <siobhanmccauley@gmail.com>
2021-11-01 16:27:19 +0000

    Update to views.py to slice top 3 bestselling products & iterate over list in index.html

--- home/views.py
+++ home/views.py
@@ -4,17 +4,10 @@ from django.shortcuts import render
 def index(request):
     """A view to return to the index page"""
 
-    Product.objects.all().order_by('-sold_items')[:3]
-
-
-    products =Product.objects.all()
-    products_list = list(products)
-    x=slice(3)
-    new_list = products_list[x]
-    print(new_list)
+    products = Product.objects.all().order_by('-sold_items')[:3]
 
     context = {
-        'new_list': new_list,
+        'products': products,
     }
 
     return render(request, 'home/index.html', context)
