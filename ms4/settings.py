b1a10df72adc0174fbce06fdcd965f1869373e2b
Siobhanmcc <siobhanmccauley@gmail.com>
2021-12-05 17:17:18 +0000

    Remove heroku from allowed_hosts

--- ms4/settings.py
+++ ms4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ms-4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS =  ['localhost']
 
 
 # Application definition
--- ms4/urls.py
+++ ms4/urls.py
@@ -16,7 +16,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-#from .views import handler404, handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +28,3 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#handler404 = 'ms4.views.handler404'
-
-#handler500 = 'ms4.views.handler500'
