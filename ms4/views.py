92609738df90ef2995814022ae0dd37cabdb2365
Siobhanmcc <siobhanmccauley@gmail.com>
2021-12-05 14:35:44 +0000

    Update to ms4>views.py

--- ms4/views.py
+++ ms4/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 def handler404(request,exception):
    # 404 error handler
+   data = {}
    return render(request, 'templates/404.html', status=404)
 
 def handler500(request):
     #500 error handler
+   data = {}
    return render(request, 'templates/500.html', status=500)
