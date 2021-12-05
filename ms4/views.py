2e3608016eaa9b6509d86ed6d8a1002413039dd6
Siobhanmcc <siobhanmccauley@gmail.com>
2021-12-05 17:04:22 +0000

    Remove 404 handlers

--- ms4/views.py
+++ ms4/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-def handler404(request,exception):
+#def handler404(request,exception):
    # 404 error handler
-   data = {}
-   return render(request, 'templates/404.html', {})
+ #  data = {}
+  # return render(request, 'templates/404.html', {})
 
-def handler500(request):
-    #500 error handler
-   data = {}
-   return render(request, 'templates/500.html', {})
+#def handler500(request):
+ #   #500 error handler
+ #  data = {}
+ #  return render(request, 'templates/500.html', {})
