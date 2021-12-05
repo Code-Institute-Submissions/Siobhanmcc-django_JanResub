7d059993d3f6fd35762b21d386cddc57b0429e78
Siobhanmcc <siobhanmccauley@gmail.com>
2021-12-05 16:26:53 +0000

    Remove } from ms4>views.py

--- ms4/views.py
+++ ms4/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def handler404(request,exception):
    # 404 error handler
    data = {}
-   return render(request, 'templates/404.html', {}})
+   return render(request, 'templates/404.html', {})
 
 def handler500(request):
     #500 error handler
